0e8fa62640289b80f078e264b6dd6f84a6ff21ea
Mark Adams <mbadams5@gmail.com>
2016-12-05 15:21:55 -0500

    Create .travis.sh

---
+++ .travis.sh
@@ -0,0 +1,24 @@
+if [ $ANALYZE = "true" ]; then
+    if [ "$CC" = "clang" ]; then
+        #scan-build -h
+        scan-build --use-cc=`which clang` --use-c++=`which clang++` cmake -G "Unix Makefiles" ..
+        scan-build -enable-checker security.FloatLoopCounter \
+          -enable-checker security.insecureAPI.UncheckedReturn \
+          --status-bugs -v \
+          make
+    else
+        cd ..
+        #cppcheck --help
+        cppcheck --template "{file}({line}): {severity} ({id}): {message}" \
+            --enable=style --force --std=c++03 \
+            --inline-suppr \
+            src/ 2> cppcheck.txt
+        if [ -s cppcheck.txt ]; then
+            cat cppcheck.txt
+            exit 1
+        fi
+    fi
+else
+    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+    make
+fi
